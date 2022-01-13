a2f9de5faf1979e487feb819dc6c84d1831afd8d
Tanveer Khazi <tanveer.ahamed000@gmail.com>
2022-01-13 21:07:52 +0530

    u

--- src/components/Posts/DetailView.jsx
+++ src/components/Posts/DetailView.jsx
@@ -1,11 +1,36 @@
 
 import React from 'react'
+import { Box, makeStyles } from '@material-ui/core'
+/*
+instead of using this we can write
+// import EditIcon  from '@mui/icons-material/Edit';
+// import DeleteIcon from '@mui/icons-material/Delete';
+*/
+import {Edit, Delete} from '@material-ui/icons';
+
+
+const useStyle=makeStyles({
+    container:{
+        padding:'0 100px'
+    },
+    image:{
+        width:'100%',
+        height:'50vh',
+        objectFit:'cover'
+    }
+})
 
 const DetailView = () => {
+    const classes=useStyle()
+    const url='https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80';
     return (
-        <div>
-            <p>hell00000000000o</p>
-        </div>
+        <Box className={classes.container}>
+            <img src={url} alt="" className={classes.image}/>
+            <Box>
+                <Edit/>
+                <Delete/>
+            </Box>
+        </Box>
     )
 }
 
